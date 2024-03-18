bccc7b40f7dc7f280ba93c0e7b2b49963a770231
Milici-Ilie <milicicristyan@yahoo.com>
2024-03-19 00:09:32 +0200

    numbers in words

--- src/printNumberInWord.java
+++ src/printNumberInWord.java
@@ -16,22 +16,20 @@ public class printNumberInWord {
 
     public static void printNumbers(int number) {
 
-        String numbers = switch (number) {
-            case 0 -> "ZERO";
-            case 1 -> "ONE";
-            case 2 -> "TWO";
-            case 3 -> "THREE";
-            case 4 -> "FOUR";
-            case 5 -> "FIVE";
-            case 6 -> "SIX";
-            case 7 -> "SEVEN";
-            case 8 -> "EIGHT";
-            case 9 -> "NINE";
-            default -> "OTHER";
-
-
-        };
-
-        System.out.println(numbers);
+        String numberInWord;
+        switch (number) {
+            case 0 -> numberInWord = "ZERO";
+            case 1 -> numberInWord = "ONE";
+            case 2 -> numberInWord = "TWO";
+            case 3 -> numberInWord = "THREE";
+            case 4 -> numberInWord = "FOUR";
+            case 5 -> numberInWord = "FIVE";
+            case 6 -> numberInWord = "SIX";
+            case 7 -> numberInWord = "SEVEN";
+            case 8 -> numberInWord = "EIGHT";
+            case 9 -> numberInWord = "NINE";
+            default -> numberInWord = "OTHER";
+        }
+        System.out.println(numberInWord);
     }
 }
