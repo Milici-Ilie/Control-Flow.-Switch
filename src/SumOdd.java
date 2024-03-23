63679d731e923ffc21628694e25d9acb405fd53b
Milici-Ilie <milicicristyan@yahoo.com>
2024-03-23 04:48:50 +0200

    SummOdd

--- src/SumOdd.java
+++ src/SumOdd.java
@@ -1,25 +1,27 @@
 public class SumOdd {
 
     public static void main(String[] args) {
-        int result = sumOdd(1, 1000);
-        System.out.println("Sum of odd numbers: " + result);
+//        SumOdd sumOdd = new SumOdd();
+//        SumOddRange sumOddRange = sumOdd.new SumOddRange();
+//        int sum = sumOddRange.sumOdd(1, 100);
+//        System.out.println("Suma numerelor impare între 1 și 100 este: " + sum);
 
+        //nu stiu cum sa apele acest metode pentru a afisa raspunsul
     }
-
 
     public class SumOddRange {
 
         public static boolean isOdd(int number) {
-            return number > 0 && number % 2 != 0;
+            return (number > 0) && (number % 2 != 0);
         }
 
         public static int sumOdd(int start, int end) {
-
-            if (start < 0 || end < 0 || end < start) {
+            if (start > end || start <= 0) {
                 return -1;
             }
 
             int sum = 0;
+
             for (int i = start; i <= end; i++) {
                 if (isOdd(i)) {
                     sum += i;
@@ -29,4 +31,30 @@ public class SumOdd {
         }
     }
 
+
+//    public class SumOddRange {
+//
+//        public static boolean isOdd(int number) {
+//            return number > 0 && number % 2 != 0;
+//        }
+//
+//        public static int sumOdd(int start, int end) {
+//
+//            if (start < 0 || end < 0 || end < start) {
+//                return -1;
+//            }
+//
+//            int sum = 0;
+//            for (int i = start; i <= end; i++) {
+//                if (isOdd(i)) {
+//                    sum += i;
+//                }
+//            }
+//            return sum;
+//        }
+//    }
+
 }
+
+// OR ANOTHER SOLUTION
+
