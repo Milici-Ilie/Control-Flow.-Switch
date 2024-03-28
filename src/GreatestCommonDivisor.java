aca380deaff822acd74a26d17bd947dced0b58ea
Milici-Ilie <milicicristyan@yahoo.com>
2024-03-29 00:11:43 +0200

    GreatestCommonDivisor

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,37 @@
+public class GreatestCommonDivisor {
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(15, 35));
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+
+        int storingDivisor = 1;// storing the divisor of the number
+
+        for (int i = 1; i <= Math.min(first, second); i++) {
+            if (first % i == 0 && second % i == 0) {
+//here we compare if the divisor number from "first" is equal to the "second" divisor number. then we want to display that divisor
+                storingDivisor = i;
+//              System.out.println(storingDivisor = i);
+            }
+        }
+
+        return storingDivisor;
+    }
+
+    //////////////////another solution /////////////////
+
+//    int min = first < second ? first : second;
+//    int gcd = 1;
+//        for (int j = 1; j <= min; j++) {
+//        if (first % j == 0 && second % j == 0) {
+//            gcd = j;
+//        }
+//    }
+//        return gcd;
+//}
+
+}
